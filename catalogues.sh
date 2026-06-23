83ee32b5c9a6c2d43f876851b8923e208f595d2e
Gopal <gopalakrishna.gunji@gmail.com>
2026-06-23 03:28:40 -0400

    actions

---
+++ catalogues.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOG_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+B="\e[34m"
+M="\e[35m"
+C="\e[36m"
+W="\e[37m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then 
+    echo -e "$R please run this script with root user access $N" | tee -a $LOG_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$R $2 ....failed $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$G $2 ....success $N" | tee -a $LOG_FILE
+    fi
+}
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "disabling nodejs default version"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "Enabling nodejs 20 version"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "installing nodejs"
+
+id roboshop &>>$LOGS_FILE
+if[ $? -ne 0 ]; then 
+
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "creating roboshop system user"
+else
+    echo -e "Roboshop user already created...$Y SKIPPING NOW $N"
+
+
+mkdir -p /app &>>$LOG_FILE
+VALIDATE $? "creating /app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
+VALIDATE $? "downloading catalogue application content"
