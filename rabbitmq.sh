8a0b810ea32f3ff1ca1bd183094ca81b13547e81
Gopal <gopalakrishna.gunji@gmail.com>
2026-06-26 14:35:13 -0400

    actions

---
+++ rabbitmq.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+MYSQL_HOST=mysql.durgagopalakrishna.online
+
+
+if [ $USERID -ne 0 ]; then 
+    echo -e "$R please run this script with root user access $N" | tee -a $LOG_FILE
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo -e "$R $2 ....failed $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$G $2 ....success $N" | tee -a $LOGS_FILE
+    fi
+}
+
+cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
+VALIDTE $? "Added rabbitmq repo"
+
+dnf install rabbitmq-server -y &>>$LOGS_FILE
+VALIDATE $? "Installing rabbitmq server"
+
+systemctl enable rabbitmq-server 
+systemctl start rabbitmq-server  &>>$LOGS_FILE
+VALIDATE $? "Enable and Starting RabbitMQ"
+
+rabbitmqctl add_user roboshop roboshop123
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+VAlIDATE $?
