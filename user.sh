6c9ed1156835a605d5680f3a204c5d3579f48a0e
Gopal <gopalakrishna.gunji@gmail.com>
2026-06-26 03:35:52 -0400

    actions

--- user.sh
+++ user.sh
@@ -37,7 +37,6 @@ VALIDATE $? "Install NodeJS"
 
 id roboshop &>>$LOGS_FILE
 
-
 if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
     VALIDATE $? "Creating system user"
